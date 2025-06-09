305679aa84a7aea6a4cc10191f3666c4ae59993a
BiggiogeraJ <jacopo.biggiogera@igenius.ai>
2025-06-09 10:29:31 +0200

    updated main(9 function for interface for question asking

--- scripts/run_query.py
+++ scripts/run_query.py
@@ -51,12 +51,12 @@ def query_rag(query_text: str):
     return response_text
 
 def main():
-    # Create CLI.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text
-    query_rag(query_text)
+    while True:
+        query_text = input("Enter your question (or type 'exit' to quit): ")
+        if query_text.lower() == 'exit':
+            break
+        else:
+            query_rag(query_text)
 
 if __name__ == "__main__":
     main()
